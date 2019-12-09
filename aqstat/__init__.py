6f158dcf6241229df8d7b6dbf35df86e5f42ae07
Gábor Vásárhelyi <vasarhelyi@collmot.com>
2019-12-09 15:01:51 +0100

    using click and not using glob

---
+++ aqstat/__init__.py
@@ -0,0 +1,2 @@
+"""A first attempt tool to test-visualize some of the air quality data
+coming from https://www.madavi.de/sensor/csvfiles.php?sensor=esp8266-11797099"""
--- aqstat/app.py
+++ aqstat/app.py
@@ -1,12 +1,6 @@
 """This is a first attempt to test-visualize some of the air quality data
 coming from https://www.madavi.de/sensor/csvfiles.php?sensor=esp8266-11797099
 
-Usage:
-
-    __file__ inputdir
-
-    where 'inputdir' is the place where the AQ .csv files are found.
-
 Note: The script is mostly a "lab" test to catalyze thinking about how this whole
 thing should be treated. See TODO-s below emerging through coding:
 
@@ -19,27 +13,22 @@ TODO:
 
 """
 
-from glob import glob
+import click
 from pathlib import Path
 
 from .aqdata import AQData
 from .plot import plot_humidity, plot_pm, plot_pm_ratio, plot_temperature, \
     plot_pm_vs_humidity, plot_pm_vs_temperature
 
-def main(argv=[]):
-    """Main entry point."""
-
-    # gt inputdir as first argument
-    if len(argv) != 1:
-        print(__doc__)
-        return
-    inputdir = Path(argv[0])
+@click.command()
+@click.argument("inputdir", type=click.Path(exists=True))
+def main(inputdir):
+    """Plot all kinds of air quality related stuff from .csv files in INPUTDIR."""
 
     # initialize empty dataset
     data = AQData()
-
     # parse all data
-    for filename in glob(str(inputdir / "*.csv")):
+    for filename in Path(inputdir).glob("*.csv"):
         print("Parsing", filename)
         data = data.merge(AQData.from_csv(filename))
 
@@ -54,7 +43,3 @@ def main(argv=[]):
     plot_pm_vs_humidity(data)
     # plot pm vs temperature data
     plot_pm_vs_temperature(data)
-
-if __name__ == "__main__":
-    import sys
-    main(sys.argv[1:])
