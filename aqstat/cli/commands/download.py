cd18c4550c49bad8fa95dcf49bf50850e2de5820
Gábor Vásárhelyi <vasarhelyi@collmot.com>
2019-12-20 15:24:54 +0100

    do not check files, skip already existing ones and download today's files in all cases. This is much faster.

--- aqstat/cli/commands/download.py
+++ aqstat/cli/commands/download.py
@@ -44,19 +44,15 @@ def download(outputdir, sensor_ids=""):
         # prepare output directory
         outdir = os.path.join(outputdir, str(sensor_id))
         os.makedirs(outdir, exist_ok=True)
-        # download files (only if size differs from local)
+        # download files (only if not existing or today's is newer)
         for filename in filelist:
-            url = os.path.join(baseurl, filename)
             outfile = os.path.join(outdir, os.path.split(filename)[1])
-            logging.info("Checking {}".format(filename))
-            r = requests.get(url, stream=True)
-            # TODO: compare current and cached ETag values instead of size
-            #       hint: https://pypi.org/project/requests-etag-cache/
-            if os.path.exists(outfile):
-                remote_size = int(r.headers["Content-Length"])
-                local_size = int(os.stat(outfile).st_size)
-                if local_size == remote_size:
-                    continue
+            # we skip existing older files and download today's again as it
+            # might be newer since last download
+            if os.path.exists(outfile) and str(datetime.today().date()) not in filename:
+                continue
+            url = os.path.join(baseurl, filename)
+            r = requests.get(url)
             with open(outfile, "wb") as f:
                 logging.info("Downloading {}".format(filename))
                 f.write(r.content)
