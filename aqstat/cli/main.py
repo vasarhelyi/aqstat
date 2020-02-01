97bdfd4c428a8ab53c6f19fb80a25caff6d6b7f4
Gábor Vásárhelyi <vasarhelyi@collmot.com>
2020-02-01 14:23:18 +0100

    one more helper script added to create metadata from luftdaten .csv and some minimal help on usage added

--- aqstat/cli/main.py
+++ aqstat/cli/main.py
@@ -15,7 +15,39 @@ import logging
 #@click.version_option()
 @click.option("-v", "--verbose", count=True, help="increase logging verbosity")
 def start(verbose=False):
-    """Do all kinds of air quality related stuff."""
+    """AQStat is a tool to analyze and visualize air quality data collected
+    under the luftdaten.info project. Main steps of usage are below.
+
+    0. First of all, you need to know at least the sensor ID or device ID of
+       the sensors you want to include in your analysis. Sensor IDs can be
+       checked on the sensors.community map when you click on data hexagons.
+       Device IDs are exposed at madavi.de archive but they are not associated
+       with a location there.
+
+    1. Any analysis or visualization is done offline on your computer, so to
+       proceed, you need to download data from archive.sensor.community or
+       madavi.de. You can do it with the 'aqstat download' command, see its
+       help for details.
+
+    2. For data analysis, visualization and to ease further download processes,
+       you have to create metadata .json files for all the devices you
+       want to analyze. An example on how a metadata .json file should look
+       (one file for each device involved in the analysis) is found in
+       aqstat\doc\examples\metadata.json
+
+       Hint: to get started, download some .csv files based on your known
+       sensor IDs and run the script
+       aqstat\scripts\convert_luftdaten_csv_to_metadata_json.py
+       to generate initial .json files for you. Then, supplement them manually
+       with proper names and further descriptors for your own convenience.
+
+    3. If all your data and metadata is available (they should be in a dedicated
+       air quality directory on your computer, where .json files are in the
+       main directory and data .csv files are organized into subdirectories
+       according to device IDs or sensor IDs), run `aqstat plot` to visualize
+       what you need. See its own help for more details.
+
+       """
 
     # setup logging
     if verbose > 1:
---
+++ aqstat/scripts/convert_luftdaten_csv_to_metadata_json.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+"""This is a helper script to convert a luftdaten.info or madavi.de .csv file
+into metadata.json files that are compatible with AQStat.
+
+The output, i.e., the metadata.json format is this one:
+
+    aqstat\doc\examples\metadata.json
+
+Note that luftdaten.info (archive.sensor.community) files are separated
+according to sensors, so multiple files belong to one measurement (typically
+one for particle matter and another one for temperature/humidity/pressure), so
+we assume (what is the usual case) that particle matter sensor ID is exactly
+temperature/humidity sensor ID -1
+
+On the other hand, madavi.de files contain all sensor data together under one
+device ID, but they do NOT contain geolocation and sensor IDs.
+
+"""
+
+import click
+from pandas import read_csv
+import json
+import os
+
+from aqstat.metadata import AQMetaData, SensorInfo, GPSCoordinate, ContactInfo
+from aqstat.parse import parse_metadata_from_filename, parse_madavi_csv, \
+    parse_sensorcommunity_csv
+
+
+@click.command()
+@click.argument("inputfile", type=click.Path(exists=True))
+@click.argument("outputdir", type=click.Path(exists=True), required=False)
+def main(inputfile="", outputdir=""):
+    """Parse .csv INPUTFILE and convert to .json in OUTPUTDIR. if OUTPUTDIR
+    is not defined, the path of INPUTFILE will be used as output.
+
+    """
+    outputdir = outputdir or os.path.split(os.path.abspath(inputfile))[0]
+
+    chip_id, sensor_id, sensor_type, date = parse_metadata_from_filename(inputfile)
+    if sensor_id:
+        data = parse_sensorcommunity_csv(inputfile)
+        lat = data["lat"][0]
+        lon = data["lon"][0]
+        if sensor_type == "dht22":
+            sensors = {
+                "pm10": SensorInfo("pm10", None, sensor_id - 1),
+                "pm2_5": SensorInfo("pm2_5", None, sensor_id - 1),
+                "temperature": SensorInfo("temperature", sensor_type, sensor_id),
+                "humidity": SensorInfo("humidity", sensor_type, sensor_id)
+            }
+        elif sensor_type == "bme280":
+            sensors = {
+                "pm10": SensorInfo("pm10", None, sensor_id - 1),
+                "pm2_5": SensorInfo("pm2_5", None, sensor_id - 1),
+                "temperature": SensorInfo("temperature", sensor_type, sensor_id),
+                "humidity": SensorInfo("humidity", sensor_type, sensor_id),
+                "pressure": SensorInfo("pressure", sensor_type, sensor_id)
+            }
+        elif sensor_type == "sds011":
+            sensors = {
+                "pm10": SensorInfo("pm10", sensor_type, sensor_id ),
+                "pm2_5": SensorInfo("pm2_5", sensor_type, sensor_id),
+                "temperature": SensorInfo("temperature", None, sensor_id + 1),
+                "humidity": SensorInfo("humidity", None, sensor_id + 1)
+            }
+        else:
+            raise ValueError("Sensor type '{}' not supported.".format(sensor_type))
+    elif chip_id:
+        data = parse_madavi_csv(inputfile)
+        lat = None
+        lon = None
+        sensors = {
+            "pm10": SensorInfo("pm10", None, None),
+            "pm2_5": SensorInfo("pm2_5", None, None),
+            "temperature": SensorInfo("temperature", None, None),
+            "humidity": SensorInfo("humidity", None, None)
+        }
+    else:
+        raise ValueError("Either chip_id or sensor_id should be valid.")
+
+    sensor_name = "{}-{}".format(chip_id, sensor_id)
+    amsl=0 # TODO: get from Google Maps API
+    agl=0
+
+    # create metadata object
+    metadata = AQMetaData(
+        name=sensor_name,
+        chip_id=chip_id,
+        sensors = sensors,
+        location=GPSCoordinate(lat, lon, amsl, agl),
+    )
+    # save to file
+    outfile = os.path.join(outputdir, sensor_name + ".json")
+    print("writing", outfile)
+    with open(outfile, 'w', encoding='utf8') as f:
+        r = json.dumps(metadata.as_dict(), indent=4, ensure_ascii=False)
+        f.write(r)
+
+if __name__ == "__main__":
+    main()
